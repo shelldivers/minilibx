d67ce4356b80a5a6249d0915815b99e83554b2b3
yhames <wjddn494@naver.com>
2024-07-23 15:26:06 +0900

    fix type depnds on Core

--- interface.swift
+++ interface.swift
@@ -270,7 +270,11 @@ public func mlx_mouse_move_swift(_ winptr:UnsafeRawPointer, _ x:Int32, _ y:Int32
 	var pt = CGPoint(x:CGFloat(x), y:CGFloat(y))
 	pt = win.convertToDisplay(pt)
 	CGWarpMouseCursorPosition(pt)
-	CGAssociateMouseAndMouseCursorPosition(Int32(1))
+	#if arch(arm64)
+		CGAssociateMouseAndMouseCursorPosition(Int32(1))	// For arm_64
+	#else
+        CGAssociateMouseAndMouseCursorPosition(UInt32(1))	// For x86_64
+    #endif
 	return Int32(0);
 }
 
